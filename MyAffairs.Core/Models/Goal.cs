0046243ede4bcd5a2e5833d3a630458a5a9b893b
Никита Осипов <whatdousthefoxsay5@gmail.com>
2018-06-07 23:58:26 +0300

    Созданы модели задач и пользователей

---
+++ MyAffairs.Core/Models/Goal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAffairs.Core.Models
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public bool Important { get; set; }
+        public bool Urgent { get; set; }
+        public bool Completed { get; set; }
+
+    }
+}
---
+++ MyAffairs.Core/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAffairs.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public virtual List<Goal> Goals { get; set; }
+
+
+        public static string GetHash(string password)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(
+            password));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
