bb9a6709153430ea2ddab15e8988224399b4aa58
Никита Осипов <whatdousthefoxsay5@gmail.com>
2018-06-08 00:31:24 +0300

    Добавил интерфейсы для хранилищ и Factory

---
+++ MyAffairs.Core/Factory.cs
@@ -0,0 +1,23 @@
+﻿using MyAffairs.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAffairs.Core
+{
+    public class Factory
+    {
+        private static Factory _instance;
+
+        public static Factory Instance => _instance ?? (_instance = new Factory());
+
+        private Factory() { }
+
+
+
+        private IStorage _storage;
+
+    }
+}
---
+++ MyAffairs.Core/Interfaces/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using MyAffairs.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAffairs.Core.Interfaces
+{
+    interface IStorage
+    {
+        IRepository<User> Users { get; }
+    }
+}
